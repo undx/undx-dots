f866a1520229f5ece2b5596fad8cc264a3841f0e
undx <egallois@talend.com>
2019-07-30 10:46:11 +0200

    qutebrowser: add qute-pass plugin bindings

--- www/.config/qutebrowser/config.py
+++ www/.config/qutebrowser/config.py
@@ -1926,4 +1926,9 @@ c.hints.selectors["code"] = [
     "pre"
 ]
 config.bind(';c', 'hint code userscript code_select.py')
-
+##
+## qute-pass
+config.bind('<z><l>',    'spawn --userscript qute-pass')
+config.bind('<z><u><l>', 'spawn --userscript qute-pass --username-only')
+config.bind('<z><p><l>', 'spawn --userscript qute-pass --password-only')
+##
